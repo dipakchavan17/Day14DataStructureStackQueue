c7df39d7ed375ca13b118aaec075daa08e666088
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-03 17:19:03 +0530

    Stack is created data is pushed

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -11,6 +11,11 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack And Queue");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
             Console.ReadLine();
         }
     }
