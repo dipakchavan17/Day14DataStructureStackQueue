a945418b8117e2cb4420616012a47a087db7a4d5
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-03 17:26:48 +0530

    peek and pop

--- StackAndQueue/StackAndQueue/LinkedList.cs
+++ StackAndQueue/StackAndQueue/LinkedList.cs
@@ -173,4 +173,3 @@ namespace StackAndQueue
 
     }
 }
-    
--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -16,6 +16,28 @@ namespace StackAndQueue
             stack.Push(30);
             stack.Push(56);
             stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.Display();
+            /*stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+           /* LinkedList linkedlist = new LinkedList();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(40);
+            linkedlist.Add(70);
+            linkedlist.Display();
+            int position = linkedlist.Search(40);
+            linkedlist.DeleteNodeAtParticularPosition(position);*/
+
             Console.ReadLine();
         }
     }
